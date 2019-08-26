93dd2ba1ca73db99c89db999680563a6fccdcada
ricardo.caif <ricardo.caif@ricardo.caif-10964>
2019-08-26 18:29:49 -0300

    fixed minor

--- tasks_manager/urls.py
+++ tasks_manager/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'create/$', views.TasksCreate.as_view(), name='tasks-create'),
-    # url(r'update/(?P<pk>[0-9]+)/$', views.TasksUpdate.as_view(), name='tasks-update'),
+    url(r'update/(?P<pk>[0-9]+)/$', views.TasksUpdate.as_view(), name='tasks-update'),
     url(r'^$', views.TasksList.as_view(), name='tasks-list'),
 ]
