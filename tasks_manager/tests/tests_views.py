7dc62d5a72228bd7fced2785b44caf5d52641c55
ricardo.caif <ricardo.caif@ricardo.caif-10964>
2019-09-04 11:14:45 -0300

    added coverage

--- tasks_manager/tests/tests_views.py
+++ tasks_manager/tests/tests_views.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 # from django.db import transaction
-# from ..views import 
+from ..views import TasksList 
 from django.contrib.auth.models import User
 
 C = Client()
@@ -11,6 +11,13 @@ class SignUpTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username='admin147', password='123456')
 
-    def test_sing_up(self):
+    def test_sign_up(self):
         response = C.post('/accounts/login/', {'username': 'admin147', 'password': '123456'})
         self.assertEqual(response.status_code, 302)
+
+
+# class TasksMangerTestCase(TestCase):
+#     def setUp(slef):
+#         self.user =
+#     def test_tasks_list(self):
+        
